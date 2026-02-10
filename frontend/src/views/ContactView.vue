1a9a00be706bf45cc703af76cadab38ef2873533
RiveroDev <samuelenriquerivero@gmail.com>
2026-02-10 17:41:37 -0500

    Finishing Tutorial 3 with Homework

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -3,7 +3,7 @@
 <template>
   <section class="mt-4 mb-4">
     <div class="max-w-7xl mx-auto">
-      <h2 class="text-3xl font-bold text-gray-800 text-center">Home Page</h2>
+      <h2 class="text-3xl font-bold text-gray-800 text-center">Contact Page</h2>
     </div>
   </section>
 </template>
